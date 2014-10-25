7cc5b6b122caf3a04915a38888268a027f71a247
SterlingVix <amelocik@gmail.com>
2014-10-24 18:26:31 -0700

    Widget is completed to specs.

--- script/Widget.js
+++ script/Widget.js
@@ -1,80 +1,128 @@
-var Widget = function(widgetType, imageCollection) {
-  this.widgetType = widgetType;
-  this.images = imageCollection;
+var Widget = function (imageArray, widgetMode) {
+  this.widgetType = widgetMode;
+  this.images = imageArray;
   this.currentImage = 0;
-  this.widgetID; // = this.setID();
-  this.divID = 'id="widget-div' + this.widgetID + '"';
-  this.imgID = 'id="widget-img' + this.widgetID + '"';
-  this.imageHTML =  this.setImageHTML();
-  this.widgetHTML = this.setInnerHTML();
-  this.divNode; // = this.setDivNode(); // the HTML node of the div containing the main image
-  this.imgNode; //  = this.setImgNode(); // the HTML node of the main image
-    
-  
-  
+  this.finalImage = this.images.length - 1;
+  this.widgetID = Widget.uniqueID++;
+  this.widgetNode; // the HTML node of the div containing the main image
+  this.imgNode; // the HTML node of the main image
 }; // end Widget()
 
-Widget.prototype.id = 0;
 
-Widget.prototype.setID = function() {
-//  return ('id="widget' + Widget.prototype.id++ + '"'); // increment unique widgetID
-  return Widget.prototype.id++; // increment unique widgetID
-}; // end setID
-
-
-
-Widget.prototype.setDivNode = function() {
-  return document.getElementById(this.divID);
-}
+/*****
+ * Give the Widget Object a constructor and unique counter for instance IDs
+ ****/
+Widget.prototype.constructor = Widget;
+Widget.uniqueID = 0;
 
 
-Widget.prototype.setImgNode = function() {
-  return document.getElementById(this.imgID);
-}
+/*****
+ * Create the main div container for the widget on the DOM
+ ****/
+Widget.prototype.createWidgetNode = function () {
+  this.widgetNode = document.createElement('div');
+  this.widgetNode.setAttribute('class', this.widgetType + '-main-div');
+  this.widgetNode.setAttribute('id', 'widget-div-' + this.widgetID);
+}; // end createWidgetNode()
 
 
-Widget.prototype.foo = function() {console.log("foo");};
+/*****
+ * Endpoint for creating image elements from widget data
+ ****/
+Widget.prototype.createImgNode = function (imageIndex) {
+  var imageNode = document.createElement('img');
+  imageNode.setAttribute('class', this.widgetType + '-img');
+  imageNode.setAttribute('id', 'widget-' + this.widgetType + '-img-' + this.widgetID);
 
-Widget.prototype.setInnerHTML = function() {
-  if (this.widgetType === 'single') { // the HTML string for the 'single'-type widget
-    return '<div class="single-main-div" ' + this.divID + '>' + this.imageHTML + '</div>';
-  } else if (this.widgetType === 'thumb') { // the HTML string for the 'thumb'-type widget
-    return '<div class="thumb-main-div" ' + this.divID + '>' + this.imageHTML + '</div><div class="thumb-scroll-div"><h1>Hello World!</h1></div>';
+  // Set initial picture
+  imageNode.setAttribute('src', this.images[imageIndex]);
+
+  return imageNode;
+}; // end createImgNode()
+
+
+/*****
+ * Create the filmstrip div container for the thumbnail widget
+ ****/
+Widget.prototype.createFilmstripNode = function () {
+  this.filmstripNode = document.createElement('div');
+  this.filmstripImages = [];
+
+  this.filmstripNode.setAttribute('id', 'widget-filmstrip-div-' + this.widgetID);
+  this.filmstripNode.setAttribute('class', 'filmstrip-div');
+
+  // create image nodes, push onto array, append to '.thumb-filmstrip-div' div
+  for (var i = 0; i <= this.finalImage; i++) {
+    this.filmstripImages.push(this.createImgNode(i));
+    this.filmstripImages[i].setAttribute('class', 'filmstrip-img');
+    this.filmstripImages[i].setAttribute('id', 'filmstrip-img-' + i);
+    this.filmstripImages[i].setAttribute('name', i);
+
+    // Add a click event listener to the image
+    this.filmstripImages[i].addEventListener('click', this.thumbImageClicked.bind(this));
+
+    // append the image to the '.thumb-filmstrip-div' div
+    this.filmstripNode.appendChild(this.filmstripImages[i]);
+  } // end for (add pictures)
+}; // end createFilmstrip()
+
+
+/*****
+ * Event action from clicking main image
+ ****/
+Widget.prototype.mainImageClicked = function (event) {
+  // Increment this.currentImage or reset
+  if (++this.currentImage > this.finalImage) {
+    this.currentImage = 0;
   }
-  
-  // default innerHTML (in case of invalid string):
-  return '<div class="widget-error" ' + this.divID + '><h2>Error! Tried to insert a widget of type "' + this.widgetType + '" here.</h2></div>';
-}; // end setInnerHTML()
 
-Widget.prototype.setImageHTML = function() {
-  if (this.widgetType === 'single') { // the HTML string for the 'single'-type image
-    return '<img class="single-main-image" ' + this.imgID + ' src="' + this.images[this.currentImage] + '">';
-  } else if (this.widgetType === 'thumb') { // the HTML string for the 'thumb'-type image
-    return '<img class="thumb-main-image" ' + this.imgID + ' src="' + this.images[this.currentImage] + '">';
+  this.updateImage(this.currentImage);
+}; // end mainImageClicked()
+
+
+/*****
+ * Event action from clicking thumbnail
+ ****/
+Widget.prototype.thumbImageClicked = function (event) {
+  // update this.currentImage
+  this.currentImage = parseInt(event.target.name)
+  this.updateImage(this.currentImage);
+}; // end thumbImageClicked()
+
+
+/*****
+ * Method to update image rendered on DOM
+ ****/
+Widget.prototype.updateImage = function (imageIndex) {
+  this.imgNode.setAttribute('src', this.images[this.currentImage]);
+}; // end updateImage()
+
+
+/*****
+ * Endpoint to place Widget object on DOM
+ ****/
+Widget.prototype.appendToNode = function (DOMElement) {
+  // Create the main widget node
+  this.createWidgetNode();
+  DOMElement.appendChild(this.widgetNode);
+
+  // Create the main image node
+  this.imgNode = this.createImgNode(this.currentImage);
+  this.widgetNode.appendChild(this.imgNode);
+
+  if (this.widgetType === 'single') {
+    
+    // Add event listener to main image, use function binding to bind this Widget object to the DOM node.
+    this.imgNode.addEventListener('click', this.mainImageClicked.bind(this));
+  } else if (this.widgetType === 'thumb') {
+    
+    // Create filmstrip
+    this.createFilmstripNode();
+    this.widgetNode.appendChild(this.filmstripNode);
+  } else {
+    return 'Error: invalid Widget of type "' + this.widgetType + '".';
   }
-  this.imgID
-  // default innerHTML (in case of invalid string):
-  return '<div class="widget-error"><h2>Error! Tried to insert a widget of type "' + this.widgetType + '" here.</h2></div>';
-}; // end setInnerHTML()
 
-
-Widget.prototype.appendToNode = function(DOMElement) {
-  this.widgetID = this.setID(); // set unique ID
-  DOMElement.innerHTML += this.widgetHTML;
-  
-  this.divNode = this.setDivNode(); // the HTML node of the div containing the main image
-  this.imgNode = this.setImgNode(); // the HTML node of the main image
+  // Return a reference of the appended object so we can capture and manipulate it
   return this;
-};
-
-
-Widget.prototype.setNodeTo = function(DOMElement) {
-  this.widgetID = this.setID(); // set unique ID
-  DOMElement.innerHTML += this.widgetHTML;
-  
-  this.divNode = this.setDivNode(); // the HTML node of the div containing the main image
-  this.imgNode = this.setImgNode(); // the HTML node of the main image
-  return this;
-};
-
-
+}; // end appendToNode()
--- script/index.js
+++ script/index.js
@@ -1,69 +1,53 @@
-document.clickFunction = function(num) {
-//  alert("clicked");
-  if (num === 1) {
-    document.widgetSingle.appendToNode(document.middleContainer);
-  } else if (num === 2) {
-    document.widgetThumb.setNodeTo(document.middleContainer);
-  } else if (num === 3) {
-    document.widgetError.appendToNode(document.middleContainer);
-  }
-}; // end clickFunction()
-  
-
-
 window.onload = function () {
-  var widgetCounter = 0;
+  console.log('Page loaded');
   
-  console.log('javascript has loaded!')
-
-  var images = ['images/0.jpg', 'images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg'];
-  var imagesJSON = {
-    0: 'images/0.jpg',
-    1: 'images/1.jpg', 
-    2: 'images/2.jpg', 
-    3: 'images/3.jpg', 
-    4: 'images/4.jpg'
-  };
-
-//  DOMElementString1 = '<div class="thumb-main-image"><img src="' + images[0] + '"></div><div class="thumb-scroll"><h1>Hello World!</h1></div>';
-//  DOMElementString2 = '<div class="single-image"><img src="' + images[1] + '"></div>';
-
-  var topContainer = document.getElementById('top-container'); // console.log(topContainer);
-  var middleContainer = document.getElementById('middle-container');
-  var bottomContainer = document.getElementById('bottom-container');
-
-//  topContainer.innerHTML += DOMElementString1;
+  // Container for all widget objects
+  var widget = [];
+  var animals = ['images/0.jpg', 
+                'images/1.jpg', 
+                'images/2.jpg', 
+                'images/3.jpg', 
+                'images/4.jpg', 
+                'images/5.jpg', 
+                'images/6.jpg', 
+                'images/7.jpg', 
+                'images/8.jpg'];
   
-  var widgetSingle = new Widget('single', images);
-  var widgetThumb = new Widget('thumb', images);
+  var magazines = ['images/a.jpg', 
+                   'images/b.jpg', 
+                   'images/c.jpg', 
+                   'images/d.jpg', 
+                   'images/e.jpg', 
+                   'images/f.jpg', 
+                   'images/g.jpg', 
+                   'images/h.jpg'];
+  
+  
 
-  document.images = ['images/0.jpg', 'images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg'];
-  document.topContainer = document.getElementById('top-container'); // console.log(topContainer);
-  document.middleContainer = document.getElementById('middle-container');
-  document.bottomContainer = document.getElementById('bottom-container');
-  document.widgetSingle = new Widget('single', images);
-  document.widgetThumb = new Widget('thumb', images);
-  document.widgetError = new Widget('something', images);
+  // Get DOM node references
+  var leftContainer = document.getElementById('left-container');
+  var rightContainer = document.getElementById('right-container');
+  
+  // Button-referenced function for adding instances to the DOM
+  document.addWidget = function(mode, containerID) {
+    var container = document.getElementById(containerID);
+    var latestWidget = widget.length;
     
+    // Select different images for the widgets
+    var images;
+    if (widget.length % 2 === 0) {
+      images = animals;
+    } else {
+      images = magazines;
+    }
+    
+    // Instantiate a new widget of mode 'mode' and push to the 'widget' array
+    widget.push( new Widget(images, mode) );
+    
+    // Append the new widget instance to the DOM
+    widget[latestWidget].appendToNode(container);
+  }; // end addWidget()
 }; // end document.onload
-
-/*
-var middleContainer = document.getElementById('middle-container');
-var images = ['images/0.jpg', 'images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg'];
-var widgetSingle = new Widget('single', images);
-var widgetThumb = new Widget('thumb', images);
-var widgetError = new Widget('something', images);
-
-widgetSingle.appendToNode(middleContainer);
-
-widgetThumb.setNodeTo(middleContainer);
-
-widgetError.appendToNode(middleContainer);
-  
-
-*/
-
-
 
 
 /*
@@ -86,59 +70,7 @@ Key Rate Limits
 */
 
 
-
-
 /*
-1) Build image gallery widget
-  a. displays a series of images
-  b. in 2 modes
-  c. Must be configurable
-  d. be OOP (support multiple instances)
-  
-2) Widget Modes:
-  a. Thumbnail mode:
-    1. Area 1: large version of current image
-    2. Area 2: scrollable filmstrip of thumbnails for each image in the gallery.
-    3. Clicking a thumbnail in the filmstrip makes that image the 'current image' and replaces previous image in Area 1.
-    
-  b. Single Image mode:
-    1. Area 1: only area:
-      a. Each image is displayed one-at-a-time
-      b. Clicking the displayed image changes to the next image in the gallery
-      c. Clicking last image cycles back to image 1.
-      
-3) Widget Config
-  a. Widget takes in a 'listOfImages' and its 'mode'
-  b. "It can take this data in any way you see fit (as a JSON string passed to a constructor function, as data-* attributes on the widget element, the images can be passed as <img> elements within the widget container, etc.)"
-
-4) ...
-
-*/
-
-
-/*
-  Gallery Widget Test
-Your task is to build an image gallery widget that displays a series of images in two different modes.  The widget must be configurable and must support multiple instances of the widget existing on the same page.
-
-
-  Widget Modes
-The widget should support two distinct modes:
-Thumbnail mode: In thumbnail mode the widget is comprised of two main areas.  The first area displays a large version of the current image.  The second area displays a scrollable filmstrip of thumbnails for each image in the gallery.  Clicking on an thumbnail in the filmstrip makes that image the current image and displays it in the first area (replacing the previously selected image.)
-
-Single Image mode: In single image mode the widget has a single area.  Each image is displayed one-at-a-time in the main area.  Clicking on the displayed image changes to the next image in the gallery.  When the last image is reached, clicking again should cycle back to the first image.
- 
-
-  Widget Configuration
-The widget takes two pieces of data for its configuration: a list of images for the gallery and its mode.  It can take this data in any way you see fit (as a JSON string passed to a constructor function, as data-* attributes on the widget element, the images can be passed as <img> elements within the widget container, etc.)
- 
-
-  Coding Requirements
-Do not use any JavaScript libraries, this widget should be written with native JavaScript and standard DOM/browser APIs.
-Document and format your code: readability matters.
-The widget should work in the current stable versions of Chrome and Safari.
-The use of Object Oriented coding practices is a plus.
- 
-
   Test Deliverables
 Deliver your project as a self-contained zip file.  Provide an HTML page with at least two instances of your widget: one for each mode, with different images.  
 */
